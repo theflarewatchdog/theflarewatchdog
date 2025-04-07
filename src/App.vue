4dd6e2ff9bef383c89e2a7c9e6e84152702727b9
Watchdog <theflarewatchdog@gmail.com>
2025-04-07 00:19:07 -0400

    better spacing

--- src/App.vue
+++ src/App.vue
@@ -9,38 +9,40 @@ import ReportButton from "./components/ReportButton.vue";
     <div class="section-container">
       <section id="home">
         <header><h1>Does this sound familiar?</h1></header>
-        <div class="squishable-iframe-container" style="aspect-ratio: 650 / 350; max-width: 650px;">
-          <iframe 
-            src="https://www.youtube.com/embed/videoseries?si=A_oNDdt3qeOVgigU&amp;list=PLdz3MBuce8onVLxxlcGvME5LL0d77uKYB" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" 
-            allowfullscreen
-          ></iframe>
-        </div>
+          <div class="squishable-iframe-container" style="aspect-ratio: 650 / 350; max-width: 650px;">
+            <iframe 
+              src="https://www.youtube.com/embed/videoseries?si=A_oNDdt3qeOVgigU&amp;list=PLdz3MBuce8onVLxxlcGvME5LL0d77uKYB" 
+              title="YouTube video player" 
+              frameborder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              referrerpolicy="strict-origin-when-cross-origin" 
+              allowfullscreen
+            ></iframe>
+          </div>
       </section>
       
       <section>
-        <p>
-          It's probably faimilar if you live in East Hamilton.
-        </p>
-        <p>
-          While it's not uncommon for our community to hear hum of industry, this noise stands out from the rest.
-        </p>
+        <div class="normal-section">
+          <p>
+            It's probably faimilar if you live in East Hamilton.
+          </p>
+          <p>
+            While it's not uncommon for our community to hear hum of industry, this noise stands out from the rest.
+          </p>
+        </div>
       </section>
 
       <section id="about">
         <div class="split-section">
-          <div>
+          <div style="flex: 1;">
             <p>
-            It's the sound that's heard when ArcelorMittal Dofasco's large gas flare is lit.
-          </p>
-          <p>
-            It rings clearly throughout neighbourhoods more than 2 km away.
-          </p>
+              It's the sound that's heard when ArcelorMittal Dofasco's large gas flare is lit.
+            </p>
+            <p>
+              It rings clearly throughout neighbourhoods more than 2 km away.
+            </p>
           </div>
-          <div class="squishable-iframe-container" style="aspect-ratio: 1; max-width: 600px;">
+          <div class="squishable-iframe-container" style="flex: 1; aspect-ratio: 1; max-width: 600px;">
             <iframe 
               src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d23512.067873812797!2d-79.800605013315!3d43.26735596398069!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDPCsDE2JzAxLjEiTiA3OcKwNDgnMTguMCJX!5e1!3m2!1sen!2sca!4v1743996679872!5m2!1sen!2sca" 
               width="600" 
@@ -55,18 +57,20 @@ import ReportButton from "./components/ReportButton.vue";
       </section>
 
       <section id="why">
-        <p>
-          If the flare is lit, record it!
-        </p>
-        <p>
-          By reporting the noise with video evidence, you are helping our community send a clear message:
-        </p>
-        <ul>
-          <li>We hear it.</li>
-          <li>We're concerned about its impact on our sleep and well-being.</li>
-          <li>We want to know whether it is compliant with the rules and regulations enforced on the rest of Hamilton's industry.</li>
-          <li>We want the cause of the noise to be investigated and fixed.</li>
-        </ul>
+        <div class="normal-section">
+          <p>
+            If the flare is lit, record it!
+          </p>
+          <p>
+            By reporting the noise with video evidence, you are helping our community send a clear message:
+          </p>
+          <ul>
+            <li>We hear it.</li>
+            <li>We're concerned about its impact on our sleep and well-being.</li>
+            <li>We want to know whether it is compliant with the rules and regulations enforced on the rest of Hamilton's industry.</li>
+            <li>We want the cause of the noise to be investigated and fixed.</li>
+          </ul>
+        </div>
       </section>
     </div>
     <ReportButton class="fixed-report-button" />
@@ -89,13 +93,23 @@ import ReportButton from "./components/ReportButton.vue";
   scroll-snap-align: start;
   display: flex;
   flex-direction: column;
-  gap: 30px;
   align-items: center;
   justify-content: center;
 }
 
 .section-container section:nth-of-type(2n) {
   background-color: #2e2e2e;
+}
+
+.normal-section {
+  max-width: 650px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+}
+
+.normal-section p {
+  text-align: center;
 }
 
 .split-section {
@@ -108,10 +122,6 @@ import ReportButton from "./components/ReportButton.vue";
   .split-section {
     flex-direction: column; /* Switch to vertical stacking */
   }
-}
-
-.split section div {
-  flex: 1;
 }
 
 .fixed-report-button {
